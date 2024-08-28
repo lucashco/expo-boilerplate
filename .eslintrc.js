ee7b4a38098bf6e0c913412d9bf79ca3c0e4407c
Lucas Castilho <lucas.h.a.oliveira@accenture.com>
2024-08-28 17:28:51 -0300

    chore: adds tanstack query linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier', 'simple-import-sort'],
+  extends: [
+    'expo',
+    'prettier',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
+  plugins: ['prettier', 'simple-import-sort', '@tanstack/query'],
   rules: {
     'prettier/prettier': 'error',
     'simple-import-sort/imports': 'error',
